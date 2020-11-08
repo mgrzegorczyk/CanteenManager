2f219712e8a15823792a1ccc0cf2f36e2bc303d1
mgrzegor <m-grzegorczyk@o2.pl>
2020-11-08 16:22:35 +0100

    user dto

---
+++ CanteenManager.Infrastructure/DTO/UserDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CanteenManager.Infrastructure.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
