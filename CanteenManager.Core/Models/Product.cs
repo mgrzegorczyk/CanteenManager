d0689e3b3b9f5a317f6496478ba10358cdec0791
mgrzegor <m-grzegorczyk@o2.pl>
2021-01-15 23:11:21 +0100

    product model - remove id

--- CanteenManager.Core/Models/Product.cs
+++ CanteenManager.Core/Models/Product.cs
@@ -4,13 +4,11 @@ namespace CanteenManager.Core.Models
 {
     public class Product
     {
-        public int Id { get; protected set; }
         public string Name { get; protected set; }
 
         protected Product() { }
         public Product(string name)
         {
-            Id = new Random().Next();
             Name = name;
         }
 
